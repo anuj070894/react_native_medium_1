16a2409290beaa73bf686fa7d675431682241cca
Anuj Kumar <anujkuma@adobe.com>
2018-06-16 23:49:29 +0530

    01 init

---
+++ index.js
@@ -0,0 +1,10 @@
+// import libraries to create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+// create the component
+
+const App = () => <Header />;
+
+// render the component
+AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+// import the necessary libraries
+import React from 'react';
+import { Text } from 'react-native';
+
+// create the component
+const Header = () => {
+  return (
+    <Text>
+      Blogs
+    </Text>
+  );
+};
+
+// make the component available to others
+export default Header;
