eeb2ec2b2a4e9f0024bda422d52d99a134895cf8
Anuj Kumar <anujkuma@adobe.com>
2018-06-17 19:31:11 +0530

    02_Init

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { AppRegistry } from 'react-native';
 import Header from './src/components/header';
 // create the component
 
-const App = () => <Header />;
+const App = () => <Header headerText="Blogs!" />;
 
 // render the component
 AppRegistry.registerComponent('albums', () => App);
